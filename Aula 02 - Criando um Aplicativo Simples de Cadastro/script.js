63045890099d6dcbd3cf02053bb6ae0c62cf087c
Gentil Junio <88815184+gentiljunio@users.noreply.github.com>
2026-02-05 16:46:28 -0300

    Add files via upload

---
+++ Aula 02 - Criando um Aplicativo Simples de Cadastro/script.js
@@ -0,0 +1,14 @@
+let nome;
+let idade;
+let cidade;
+
+nome = prompt("Digite seu nome:");
+idade = prompt("Digite sua idade:");
+cidade = prompt("Digite sua cidade:");
+
+alert(
+  "Dados do usuário:\n" +
+  "Nome: " + nome + "\n" +
+  "Idade: " + idade + " anos\n" +
+  "Cidade: " + cidade
+);
